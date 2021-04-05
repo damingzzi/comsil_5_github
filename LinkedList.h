2a3eb97f522305ab2350d34fcdf34c7e33d2935c
damingzzi <jdm0806@u.sogang.ac.kr>
2021-04-05 16:57:12 +0900

    과제 test3

--- LinkedList.h
+++ LinkedList.h
@@ -8,7 +8,7 @@ template <class T>
 class Node {
 public:
 	T data;
-	Node* link;
+	Node* link; //다음 node를 가리키는 pointer
 	Node(T element) {
 		data = element;
 		link = 0;
@@ -18,10 +18,10 @@ public:
 template <class T>
 class LinkedList {
 protected:
-	Node<T>* first;
+	Node<T>* first; //가장 마지막에 들어온 node를 first로 한다.
 	int current_size;
 public:
-	LinkedList() {
+	LinkedList() { //생성자는 first와 current_size를 모두 0으로한다.
 		first = 0;
 		current_size = 0;
 	};
@@ -37,12 +37,12 @@ public:
 
 
 template <class T>
-void LinkedList<T>::Insert(T element) {
+void LinkedList<T>::Insert(T element) { 
 	Node<T>* newnode = new Node<T>(element);
 
-	newnode->link = first;
+	newnode->link = first; //insert하면 first를 변경시켜준다.
 	first = newnode;
-	current_size++;
+	current_size++; //current_size를 증가시키는 것을 잊지말자.
 }
 
 template <class T>
@@ -54,7 +54,8 @@ bool LinkedList<T>::Delete(T& element) {
 	Node<T>* previous = 0;
 
 	while (1) {
-		if (current->link == 0) { 
+		if (current->link == 0) { //linked list에서 가장 첫번째로 들어온 요소를 삭제한다.
+					//따라서 current가 가리키는 곳이 null pointer이도록 끝까지 탐색한다.
 			if (previous)
 				previous->link = current->link;
 			else
@@ -89,7 +90,7 @@ void LinkedList<T>::Print() {
 				index++;
 			}
 		}
-		cout << endl;
+		cout << '\n';
 	}
 }
 
